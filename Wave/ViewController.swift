0c9087a4b2a2f3c8fb1a50ddd4299be9d0aae0ad
ttdp <ttdp@foxmail.com>
2021-11-04 20:10:05 +0800

    Initial Commit

---
+++ Wave/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wave
+//
+//  Created by Tian Tong on 2021/11/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
