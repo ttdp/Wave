c54beb3415a755b429744ccca1e3addc0e0d9fe1
ttdp <tiantong.dev@gmail.com>
2021-11-06 19:33:13 +0800

    add mic play undo image

--- Wave/Views/SoundViewController.swift
+++ Wave/Views/SoundViewController.swift
@@ -38,7 +38,7 @@ class SoundViewController: UIViewController {
     var currentState: AudioRecodingState = .ready {
         didSet {
             audioVisualizationView.audioVisualizationMode = currentState.audioVisualizationMode
-            clearButton.isHidden = currentState == .ready || currentState == .playing || currentState == .recording
+            undoButton.isHidden = currentState == .ready || currentState == .playing || currentState == .recording
         }
     }
     
@@ -70,7 +70,7 @@ class SoundViewController: UIViewController {
         }
         
         viewModel.audioDidFinish = { [weak self] in
-            self?.playButton.setTitle("Play", for: .normal)
+            self?.playButton.setImage(UIImage(named: "wave_play"), for: .normal)
             self?.currentState = .recorded
             self?.audioVisualizationView.stop()
             self?.isPlaying = false
@@ -81,28 +81,30 @@ class SoundViewController: UIViewController {
     
     lazy var recordButton: UIButton = {
         let button = UIButton(type: .system)
-        button.tintColor = .purple
+        button.tintColor = UIColor.systemGray
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
-        button.setTitle("Record", for: .normal)
+        button.setImage(UIImage(named: "wave_mic"), for: .normal)
         button.addTarget(self, action: #selector(handleRecord), for: .touchUpInside)
         return button
     }()
     
     lazy var playButton: UIButton = {
         let button = UIButton(type: .system)
-        button.tintColor = .purple
+        button.tintColor = UIColor.systemBlue
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
-        button.setTitle("Play", for: .normal)
+        button.setImage(UIImage(named: "wave_play"), for: .normal)
         button.addTarget(self, action: #selector(handlePlay), for: .touchUpInside)
+        button.alpha = 0
         return button
     }()
     
-    lazy var clearButton: UIButton = {
+    lazy var undoButton: UIButton = {
         let button = UIButton(type: .system)
-        button.tintColor = .purple
+        button.tintColor = UIColor.systemGray
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 24)
-        button.setTitle("Clear", for: .normal)
+        button.setImage(UIImage(named: "wave_undo"), for: .normal)
         button.addTarget(self, action: #selector(handleClear), for: .touchUpInside)
+        button.isHidden = true
         return button
     }()
     
@@ -118,27 +120,29 @@ class SoundViewController: UIViewController {
         audioVisualizationView.backgroundColor = .secondarySystemBackground
         
         view.addSubview(recordButton)
-        view.addConstts(format: "H:|[v0]|", views: recordButton)
+        view.addConstts(format: "H:[v0(50)]", views: recordButton)
         view.addConstts(format: "V:[v0(50)]", views: recordButton)
+        recordButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         recordButton.topAnchor.constraint(equalTo: audioVisualizationView.bottomAnchor).isActive = true
         
         view.addSubview(playButton)
-        view.addConstts(format: "H:|[v0]|", views: playButton)
+        view.addConstts(format: "H:[v0(50)]", views: playButton)
         view.addConstts(format: "V:[v0(50)]", views: playButton)
-        playButton.topAnchor.constraint(equalTo: recordButton.bottomAnchor).isActive = true
+        playButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        playButton.topAnchor.constraint(equalTo: audioVisualizationView.bottomAnchor).isActive = true
         
-        view.addSubview(clearButton)
-        view.addConstts(format: "H:|[v0]|", views: clearButton)
-        view.addConstts(format: "V:[v0(50)]", views: clearButton)
-        clearButton.topAnchor.constraint(equalTo: playButton.bottomAnchor).isActive = true
-        
+        view.addSubview(undoButton)
+        view.addConstts(format: "H:[v0(35)]", views: undoButton)
+        view.addConstts(format: "V:[v0(35)]", views: undoButton)
+        undoButton.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 100).isActive = true
+        undoButton.centerYAnchor.constraint(equalTo: playButton.centerYAnchor).isActive = true
     }
     
     // MARK: - Action
     
     @objc func handleRecord() {
         if !isRecording {
-            recordButton.setTitle("Recording", for: .normal)
+            recordButton.tintColor = UIColor.systemRed
             
             do {
                 try viewModel.resetRecording()
@@ -161,7 +165,9 @@ class SoundViewController: UIViewController {
                 self?.chronometer?.start()
             }
         } else {
-            recordButton.setTitle("Record", for: .normal)
+            recordButton.tintColor = UIColor.systemGray
+            recordButton.alpha = 0
+            playButton.alpha = 1
             
             chronometer?.stop()
             chronometer = nil
@@ -183,7 +189,7 @@ class SoundViewController: UIViewController {
     
     @objc func handlePlay() {
         if !isPlaying {
-            playButton.setTitle("Playing", for: .normal)
+            playButton.setImage(UIImage(named: "wave_pause"), for: .normal)
             
             do {
                 let duration = try viewModel.startPlaying()
@@ -194,7 +200,7 @@ class SoundViewController: UIViewController {
                 showAlert(with: error)
             }
         } else {
-            playButton.setTitle("Play", for: .normal)
+            playButton.setImage(UIImage(named: "wave_play"), for: .normal)
             
             do {
                 try viewModel.pausePlaying()
@@ -213,6 +219,9 @@ class SoundViewController: UIViewController {
             try viewModel.resetRecording()
             audioVisualizationView.reset()
             currentState = .ready
+            
+            recordButton.alpha = 1
+            playButton.alpha = 0
         } catch {
             showAlert(with: error)
         }
